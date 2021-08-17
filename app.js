d78b5138e84c898a71748d067acf1bcf933c07f6
Falence Lemungoh <falencelemungoh@gmail.com>
2021-08-17 16:48:49 +0100

    Setup pug and static files settings

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ const viewRouter = require('./routes/viewRoutes')
 const contactRouter = require('./routes/contactRoutes')
 
 const app = express()
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
+// GLOBAL MIDDLEWARES
 app.use(express.json())
 
+// serve static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+// Routes
 app.use('/', viewRouter)
 app.use('/api/contacts', contactRouter)
 
